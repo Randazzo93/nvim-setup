cd91659f31ccc8e5b99762e5c355e3b16bb77faa
Thomas Randazzo <thomas.randazzo@brightside.com>
2024-08-21 11:06:28 -0500

    updated

--- lua/theprimeagen/lazy/colors.lua
+++ lua/theprimeagen/lazy/colors.lua
@@ -1,89 +1,83 @@
-function ColorMyPencils(color)
-	color = color or "rose-pine-moon"
-	vim.cmd.colorscheme(color)
-
-	vim.api.nvim_set_hl(0, "Normal", { bg = "none" })
-	vim.api.nvim_set_hl(0, "NormalFloat", { bg = "none" })
-end
-
 return {
+    "rose-pine/neovim",
+    name = "rose-pine",
+    config = function()
+        require('rose-pine').setup({
+            variant = "auto", -- auto, main, moon, or dawn
+            dark_variant = "main", -- main, moon, or dawn
+            dim_inactive_windows = false,
+            extend_background_behind_borders = true,
 
-    {
-        "erikbackman/brightburn.vim",
-    },
+            enable = {
+                terminal = true,
+                legacy_highlights = true, -- Improve compatibility for previous versions of Neovim
+                migrations = true, -- Handle deprecated options automatically
+            },
 
-    {
-        "folke/tokyonight.nvim",
-        lazy = false,
-        opts = {},
-        config = function()
-            ColorMyPencils()
-        end
-    },
-    {
-        "ellisonleao/gruvbox.nvim",
-        name = "gruvbox",
-        config = function()
-            require("gruvbox").setup({
-                terminal_colors = true, -- add neovim terminal colors
-                undercurl = true,
-                underline = false,
+            styles = {
                 bold = true,
-                italic = {
-                    strings = false,
-                    emphasis = false,
-                    comments = false,
-                    operators = false,
-                    folds = false,
-                },
-                strikethrough = true,
-                invert_selection = false,
-                invert_signs = false,
-                invert_tabline = false,
-                invert_intend_guides = false,
-                inverse = true, -- invert background for search, diffs, statuslines and errors
-                contrast = "", -- can be "hard", "soft" or empty string
-                palette_overrides = {},
-                overrides = {},
-                dim_inactive = false,
-                transparent_mode = false,
-            })
-        end,
-    },
-    {
-        "folke/tokyonight.nvim",
-        config = function()
-            require("tokyonight").setup({
-                -- your configuration comes here
-                -- or leave it empty to use the default settings
-                style = "storm", -- The theme comes in three styles, `storm`, `moon`, a darker variant `night` and `day`
-                transparent = true, -- Enable this to disable setting the background color
-                terminal_colors = true, -- Configure the colors used when opening a `:terminal` in Neovim
-                styles = {
-                    -- Style to be applied to different syntax groups
-                    -- Value is any valid attr-list value for `:help nvim_set_hl`
-                    comments = { italic = false },
-                    keywords = { italic = false },
-                    -- Background styles. Can be "dark", "transparent" or "normal"
-                    sidebars = "dark", -- style for sidebars, see below
-                    floats = "dark", -- style for floating windows
-                },
-            })
-        end
-    },
+                italic = true,
+                transparency = false,
+            },
 
-    {
-        "rose-pine/neovim",
-        name = "rose-pine",
-        config = function()
-            require('rose-pine').setup({
-                disable_background = true,
-                styles = {
-                    italic = false,
-                },
-            })
-        end
-    },
+            groups = {
+                border = "muted",
+                link = "iris",
+                panel = "surface",
 
+                error = "love",
+                hint = "iris",
+                info = "foam",
+                note = "pine",
+                todo = "rose",
+                warn = "gold",
 
+                git_add = "foam",
+                git_change = "rose",
+                git_delete = "love",
+                git_dirty = "rose",
+                git_ignore = "muted",
+                git_merge = "iris",
+                git_rename = "pine",
+                git_stage = "iris",
+                git_text = "rose",
+                git_untracked = "subtle",
+
+                h1 = "iris",
+                h2 = "foam",
+                h3 = "rose",
+                h4 = "gold",
+                h5 = "pine",
+                h6 = "foam",
+            },
+
+            pallete = {
+                -- Override the builtin palette per variant
+                -- moon = {
+                --     base = '#18191a',
+                --     overlay = '#363738',
+                -- },
+            },
+
+            highlight_groups = {
+                -- Comment = { fg = "foam" },
+                -- VertSplit = { fg = "muted", bg = "muted" },
+            },
+
+            before_highlight = function(group, highlight, palette)
+                -- Disable all undercurls
+                -- if highlight.undercurl then
+                --     highlight.undercurl = false
+                -- end
+                --
+                -- Change palette colour
+                -- if highlight.fg == palette.pine then
+                --     highlight.fg = palette.foam
+                -- end
+            end,
+
+        })
+
+    vim.cmd("colorscheme rose-pine")
+    end,
 }
